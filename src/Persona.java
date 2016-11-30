5cd8fa019fcfa61a66aa708a6973d81a07a20db7
Javier Rotger Lebrón <fjrotgerl@gmail.com>
2016-11-30 21:32:46 +0100

    Afegim sexe

---
+++ src/Persona.java
@@ -0,0 +1,43 @@
+/**
+ * Created by fjrotgerl on 23/11/16.
+ */
+public class Persona {
+
+    private String dni;
+    private String nom;
+    private String cognom;
+    private String sexe;
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCognom() {
+        return cognom;
+    }
+
+    public void setCognom(String cognom) {
+        this.cognom = cognom;
+    }
+
+    public String getSexe() {
+        return sexe;
+    }
+
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
+
+}
